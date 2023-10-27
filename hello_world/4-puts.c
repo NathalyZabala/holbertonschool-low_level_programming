44ed77c26926d0f3abb1b24208b7075816dc2da7
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-10-26 20:39:45 -0700

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/**
+ * main - name of the function
+ *
+ * This cunction prints a texts using puts
+ *
+ * Return: Always 0
+ */
+
+#include <studio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+	
+}
