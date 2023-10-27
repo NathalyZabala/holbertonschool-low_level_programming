536620e1ccdc318931682a560dc7c89e0f637654
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-10-26 21:04:55 -0700

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/**
+ * main - function's name, prints a text
+ *
+ * this function prints a text using printf
+ *
+ * Return: Always 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
