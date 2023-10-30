485aa5ce772492386e405f444ff8557f3e26f275
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-10-30 16:19:37 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this code prints the alphabet in lowercase ussing putchar
+ *
+ * putchar prints just one character
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	/**
+	 * n is the alphabet letter
+	 *
+	 * for bucle goes through the aplhabet letters
+	 */
+	char n;
+	for (n = 'a'; n <= 'z'; n++) {
+		putchar(n);
+	}
+	return (0);
+}
