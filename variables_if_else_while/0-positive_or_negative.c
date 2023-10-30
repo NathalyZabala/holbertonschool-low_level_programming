3f960e4def69c27a1b51448e2b073dd7cedc5c33
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-10-30 13:52:11 -0700

    printf("%d \n", x);

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,13 @@ int main(void)
 	srand(time(0));
 	n =  rand() - RAND_MAX / 2;
 
+	printf("%d \n", n);
+
 	if (n > 0)
-		printf("%d is positive\n", n);
+		printf("is positive \n");
 	else if (n < 0)
-		printf("%d is negative\n", n);
+		printf("is negative\n");
 	else
-		printf("%d is zero\n", n);
+		printf("is zero \n");
 	return (0);
 }
