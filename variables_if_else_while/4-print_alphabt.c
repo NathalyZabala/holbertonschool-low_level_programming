594e111cde1865f1de94c8bf34fc29464fa5f317
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-10-30 16:53:08 -0700

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this code Write a program that prints the alphabet in lowercase
+ * but q and e
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n ='a'; n <= 'z'; n++) {
+		if ( n != 'q' && n != 'e');
+		putchar(n);
+	}
+	
+	putchar ('\n');
+
+	return (0);	
+}
