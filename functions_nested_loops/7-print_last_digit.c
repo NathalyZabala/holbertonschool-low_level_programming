8142c528066eebb3dcbd040e47fac4898ee37593
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-10-31 15:47:59 -0700

    7-print_last_digit.c, 5

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -14,7 +14,11 @@ int print_last_digit(int n)
 	int r;
 
 	r = n % 10;
-	
+
+	/**
+	 * si el numero es negativo, se multiplica por -1 para convertirlo a
+	 * positivo
+	 */
 	if (r < 0)
 		r = r * -1;
 
