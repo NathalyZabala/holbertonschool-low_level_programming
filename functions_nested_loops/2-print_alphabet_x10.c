ac1c8d8ac1a86bd007ceda22aae6b167e831b037
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-10-31 11:41:30 -0700

    2-print_alphabet_x10.c, n

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -14,9 +14,9 @@ void print_alphabet_x10(void)
 	 */
 	int n, m;
 
-	for (n = 0; n < 10; n++)
+	for (m = 0; m <= 10; m++)
 	{
-		for (m = 'a'; m <= 'z'; m++)
+		for (n = 'a'; n <= 'z'; n++)
 		       _putchar(n);
 	
 		_putchar('\n');
