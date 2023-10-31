166b8b5dcfa0786e2c49ed71c3b68e2e621174ef
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-10-30 18:50:16 -0700

    8-print_base16.c, comillas

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	char n;
 
-	for (n = 0; n <= 15; n++) {
-		if (n > 10) 
+	for (n = '0'; n <= '15'; n++) {
+		if (n > '10') 
 		{
 			putchar('0' + n);
 		}
 		else
 		{
-			putchar('a' + (n - 10));
+			putchar('a' + (n - '10'));
 		}
 	}
 
