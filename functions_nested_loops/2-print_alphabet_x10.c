e104a099627677a9533e3ebfd8fd75a58c99fe4e
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-10-31 11:36:08 -0700

    2-print_alphabet_x10.c, int

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,13 +12,13 @@ void print_alphabet_x10(void)
 	 * n representa la cantidad de veces que se va a repetir el ciclo
 	 * m representa la iteración del alfabeto
 	 */
-	char n, m;
+	int n, m;
 
 	for (n = 0; n < 10; n++)
 	{
 		for (m = 'a'; m <= 'z'; m++)
-		       _putchar(n);	
+		       _putchar(n);
+	
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
