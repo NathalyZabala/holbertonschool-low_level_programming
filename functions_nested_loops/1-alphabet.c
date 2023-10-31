9e7fb7df2876872b1e54bcc240439787d6a37cad
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-10-31 10:57:17 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - This code  function that prints the alphabet, in lowercase.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+
+	_putchar('\n'):
+
+	return (0);
+}
