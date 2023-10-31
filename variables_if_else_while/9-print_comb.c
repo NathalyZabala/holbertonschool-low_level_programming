0fa98c4bd9bec264387338d792841ab7a8b7e098
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-10-30 19:35:44 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <studio.h>
+
+/**
+ * main - this code prints all possible combinations of single-digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char n;
+
+	while (n <= 9)
+	{
+		/**
+		 * sumar 0 al valor de x convierte el valor numerico en en
+		 * el caracter correspondiente
+		 */
+		putchar(n + '0');
+
+		if (x <= 8)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		x++;
+
+	}
+
+	return (0);
+}
