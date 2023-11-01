d8aed4f0b152d95b406f537d88f9153cd7cc7311
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-10-31 18:03:06 -0700

    11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Write a function that prints all natural numbers from n to 98
+ *
+ * @n: number 
+ *
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		_putchar(44);
+		_putchar(32);
+	}
+}
