bfa7cf88ad1dd36919a16f04643b8665a92a68ba
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-01 08:41:42 -0700

    11-print_to_98.c, 7

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,30 +13,30 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n > 98; n++)
+		if (n > 98)
 		{
-			if (n == 98)
-			{
-				printf("%d\n", n);
-			}
-			else
+			for (; n > 98; n--)
 			{
 				printf("%d, ", n);
 			}
+		}
+		if (n == 98)
+		{
+			printf("%d\n", n);
 		}
 	}
-	else
+	if (n < 97)
 	{
-		for (; n >= 98; n--)
+		if (n < 98)
 		{
-			if (n == 98)
-			{
-				printf("%d\n", n);
-			}
-			else
+			for (; n < 98; n++)
 			{
 				printf("%d, ", n);
 			}
 		}
-	}	
+		if (n == 98)
+		{
+			printf("%d\n", n);
+		}
+	}
 }
