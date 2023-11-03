244158f17b03365f7dd18488eeec36d17a1d9d79
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-03 10:11:35 -0700

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9, followed by a new line.
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if ((n != 2) &6 (n != 4)
+			_putchar(n);
+	}
+	_putchar('\n');
+}
+
