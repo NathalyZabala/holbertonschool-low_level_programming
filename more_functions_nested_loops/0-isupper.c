e41712c3e10b152c65196b89b26a1bccd9e0bcdb
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-03 09:11:51 -0700

    0-isupper.c, 1

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,13 +4,15 @@
 /**
  * _isupper - function that checks for uppercase character.
  *
+ * @c: character
+ *
  * Return: Always 0.
  */
 
 int _isupper(int c)
 {
-	if ((c > 'A') && (c < 'Z'))
+	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
-	
+
 	return (0);
 }
