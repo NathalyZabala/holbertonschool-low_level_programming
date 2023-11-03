701aa002597794c71b6b234032f27b18272f8896
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-03 10:50:12 -0700

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+	int times = 10;
+
+	for (j = 0; j < times; j++)
+	{
+		for (i = 0; <= 14; i++)
+			_putchar(i);
+			_putchar('\n');
+	}
+	_putchar(j);
+}
