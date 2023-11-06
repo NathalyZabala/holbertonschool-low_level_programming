1923753abbeb0cd5286f8ca7e91bf8fae3a0ca9b
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-06 15:33:47 -0800

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ *
+ * @str: string
+ *
+ * Return: 0
+ */
+
 void _puts(char *str)
 {
-	_puts(str);
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }
