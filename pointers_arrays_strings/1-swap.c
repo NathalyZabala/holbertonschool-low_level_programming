9949ce9a45c843da08faf7306322271ac64b711e
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-06 13:06:03 -0800

    1-swap.c, 2

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -4,12 +4,16 @@
 /**
  * swap_int - function that swaps the values of two integers.
  *
+ * @a: entero
+ * @b: entero
+ *
  * Return: 0
  */
 
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c = *a;
 	*a = *b;
 	*b = c;
