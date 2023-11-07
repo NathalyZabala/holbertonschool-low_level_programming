b3aa1fa40027ae0892b33d6a089f7f8034193251
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-07 10:40:01 -0800

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints the second half of a string
+ *
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+
+	if (j % 2 == 1)
+	{
+		i = (j - 1) / 2;
+		i += 1;
+	}
+	else
+	{
+		i = j / 2;
+	}
+
+	for (; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
