9a2e4df395a9d9b8470afee6fa4897438aa94483
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-09 07:34:15 -0800

    0-strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
+ * *_strcat - function that concatenates two strings
+ *
+ * @dest: destination
+ * @src: source
  */
 
 char *_strcat(char *dest, char *src)
 {
-	_strcat(*dest, *src);
+	int dest_len = 0, i;
 
-	return (*dest);
+	while (dest[dest_len])
+		dest_len++;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+	}
+
+	dest[dest_len] = '\0';
+	return (dest);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -11,3 +16,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
+#endif
