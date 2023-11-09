90680f489cb228a4b4c3557ff0447f99e398833d
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-09 08:28:39 -0800

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strncat - function that concatenates two strings
+ *
+ * @dest: destination
+ * @src: source
+ * @n: bytes from src to dest
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	while (dest[i++])
+		dest_len++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[dest_len++] = src[i];
+
+	return (dest);
+}
