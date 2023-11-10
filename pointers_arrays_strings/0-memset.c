860aa9f7087a5637e18d3541a1666eda13e32940
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-10 08:54:49 -0800

    0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memset - function that fills memory with a constant byte.
+ *
+ * @i: index
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
