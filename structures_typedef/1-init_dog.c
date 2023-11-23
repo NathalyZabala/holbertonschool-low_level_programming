c5263c19966a031d4b4d9534dd40687a84611196
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-23 08:55:18 -0800

    1-init_dog.c, 3

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,7 +1,9 @@
 #include "dog.h"
 #include <stdlib.h>
 
-/** init_dog - function that initialize a variable of type struct dog
+/** 
+ * init_dog - function that initialize a variable of type struct dog
+ *
  * @d: dog structure
  * @name: ...
  * @age: ...
