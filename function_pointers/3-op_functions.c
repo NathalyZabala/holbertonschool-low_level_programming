84fff51676f20c7b53a60aa3d11cb10760680cf6
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-24 10:45:19 -0800

    3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "calc.h"
+
+/**
+ * op_add -  returns the sum of a and b
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub: returns sub of a and b
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul: returns mul of a and b
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns div of a and b
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_div - returns a mod b
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
