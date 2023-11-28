d406dc488c3339c185b6c5d4abfea8af2b335e5f
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-28 09:03:20 -0800

    1-print_numbers.c, 4

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,7 +2,7 @@
 #define _HEADER_
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.>
+#include <stdlib.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
