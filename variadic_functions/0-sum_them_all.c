06aeece019f27fdc611c6d158c634b41f7615b7c
Nathaly Zabala <nathalyzabala98@outlook.com>
2023-11-28 08:37:58 -0800

    0-sum_them_all.c, 3

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,22 +11,24 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int result;
-	result = 0;
-
-	if (n == 0)
-		return (0);
-
 	va_list args;
 
-	va_start(args, n);
+	unsigned int i = 0;
+	int result = 0;
 
-	for (int i = 0; i < n; i++)
+	if (n != 0)
 	{
-		result += va_arg(args, int);
+		va_start(args, n);
+
+		for (i = 0; i < n; i++)
+		{
+			result += va_arg(args, int);
+		}
+
+		va_end(args);
+
+		return (result);
 	}
 
-	va_end(args);
-
-	return (result);
+	return (0);
 }
